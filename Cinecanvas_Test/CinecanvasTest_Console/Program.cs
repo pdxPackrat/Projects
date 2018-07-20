a3fe1542f43b2788ecf5ea3b591b070deffcba6f
Michael Christenson <mdc88@pdxchristenson.com>
2018-07-20 14:38:05 -0700

    Current work on the Cinecanvas stuff

---
+++ Cinecanvas_Test/CinecanvasTest_Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace CinecanvasTest_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(SubtitleReelType));
+            TextReader reader = new StreamReader("Whitney_23.976_Captions_DCinema2010.xml");
+            object obj = deserializer.Deserialize(reader);
+            SubtitleReelType XmlData = (SubtitleReelType)obj;
+            reader.Close();
+
+            // SubtitleReelTypeFont data = XmlData.SubtitleList[0];
+
+            //SubtitleType[] subtitles = (SubtitleType[]) XmlData.SubtitleList.Su
+            // SubtitleType data = (SubtitleType) XmlData.SubtitleList;
+
+            SubtitleType mdc = (SubtitleType) XmlData.SubtitleList.
+
+            Console.WriteLine(" All Done ");
+        }
+        
+    }
+}
---
+++ Cinecanvas_Test/CinecanvasTest_Console/ProludioCinecanvasSubtitle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CinecanvasTest_Console
+{
+    class Subtitle
+    {
+        public int SpotNumber;
+        TimeSpan FadeUpTime = new TimeSpan(0, 0, 0);
+        TimeSpan FadeDownTime = new TimeSpan(0, 0, 0);
+        TimeSpan TimeIn = new TimeSpan(0, 0, 0);
+        TimeSpan TimeOut = new TimeSpan(0, 0, 0);
+    }
+
+    class SubtitleText
+    {
+        string subtitleText;
+
+        int vPosition;
+        int vAlign;
+        int hAlign;
+        int Direction;
+    }
+}
