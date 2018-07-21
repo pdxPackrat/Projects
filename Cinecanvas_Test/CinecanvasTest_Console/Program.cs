b6b119bad8e11407688fe31e6b974ec9f0d57935
Michael Christenson <mdc88@pdxchristenson.com>
2018-07-20 21:56:37 -0700

    Working reader so far
    Have a working reader so far that doesn't have any timing logic yet

--- Cinecanvas_Test/CinecanvasTest_Console/Program.cs
+++ Cinecanvas_Test/CinecanvasTest_Console/Program.cs
@@ -14,18 +14,41 @@ namespace CinecanvasTest_Console
     {
         static void Main(string[] args)
         {
-            XmlSerializer deserializer = new XmlSerializer(typeof(SubtitleReelType));
+            // XmlSerializer deserializer = new XmlSerializer(typeof(SubtitleList));
+            // XmlSerializer deserializer = new XmlSerializer(typeof(SubtitleReelType));
+            XmlSerializer deserializer = new XmlSerializer(typeof(SubtitleReel));
+
             TextReader reader = new StreamReader("Whitney_23.976_Captions_DCinema2010.xml");
+
             object obj = deserializer.Deserialize(reader);
-            SubtitleReelType XmlData = (SubtitleReelType)obj;
+
+            // SubtitleList XmlData = (SubtitleList)obj;
+            // SubtitleReelType XmlData = (SubtitleReelType)obj;
+            SubtitleReel XmlData = (SubtitleReel)obj;
+
             reader.Close();
 
             // SubtitleReelTypeFont data = XmlData.SubtitleList[0];
 
             //SubtitleType[] subtitles = (SubtitleType[]) XmlData.SubtitleList.Su
             // SubtitleType data = (SubtitleType) XmlData.SubtitleList;
+            // FontType = (FontType) XmlData.SubtitleList.
 
-            SubtitleType mdc = (SubtitleType) XmlData.SubtitleList.
+            int result = XmlData.subtitleList.font.subtitle.Count;
+
+            Console.WriteLine($"Total Subtitles Entries not counting multiple lines: {result}");
+
+            foreach(Subtitle subtitle in XmlData.subtitleList.font.subtitle)
+            {
+                int spot = subtitle.SpotNumber;
+                Console.WriteLine($"SpotNumber: {spot}");
+
+                foreach (Text text in subtitle.text)
+                {
+                    int pos = text.vPosition;
+                    Console.WriteLine($"{pos}: {text.subtitleText}");
+                }
+            }
 
             Console.WriteLine(" All Done ");
         }
--- Cinecanvas_Test/CinecanvasTest_Console/ProludioCinecanvasSubtitle.cs
+++ Cinecanvas_Test/CinecanvasTest_Console/ProludioCinecanvasSubtitle.cs
@@ -4,25 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace CinecanvasTest_Console
 {
-    class Subtitle
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://www.smpte-ra.org/schemas/428-7/2010/DCST")]
+    [System.Xml.Serialization.XmlRootAttribute("SubtitleReel", Namespace = "http://www.smpte-ra.org/schemas/428-7/2010/DCST", IsNullable = false)]
+    public class SubtitleReel
     {
-        public int SpotNumber;
-        TimeSpan FadeUpTime = new TimeSpan(0, 0, 0);
-        TimeSpan FadeDownTime = new TimeSpan(0, 0, 0);
-        TimeSpan TimeIn = new TimeSpan(0, 0, 0);
-        TimeSpan TimeOut = new TimeSpan(0, 0, 0);
+        [XmlElement ("Id")]
+        public string id { get; set; }
+
+        [XmlElement("TimeCodeRate")]
+        public string timeCodeRate { get; set; }
+
+        [XmlElement("LoadFont")]
+        public string loadFont { get; set; }
+
+        [XmlElement("SubtitleList")]
+        public SubtitleList subtitleList { get; set; }
+    }
+    public class SubtitleList
+    {
+        [XmlElement("Font")]
+        public Font font { get; set; }
     }
 
-    class SubtitleText
+    public class Font
     {
-        string subtitleText;
+        [XmlAttribute("ID")]
+        public string id { get; set; }
 
-        int vPosition;
-        int vAlign;
-        int hAlign;
-        int Direction;
+        [XmlAttribute("Size")]
+        public string size { get; set; }
+
+        [XmlElement ("Subtitle")]
+        public List<Subtitle> subtitle { get; set; }
+    }
+
+    public class Subtitle
+    {
+        [XmlAttribute("SpotNumber")]
+        public int SpotNumber { get; set; }
+
+        [XmlAttribute("TimeIn")]
+        public string TimeIn { get; set; }
+
+        [XmlAttribute("TimeOut")]
+        public string TimeOut { get; set; }
+
+        [XmlElement("Text")]
+        public List<Text> text { get; set; }
+    }
+
+    public class Text
+    {
+        [XmlAttribute("Vposition")]
+        public int vPosition { get; set; }
+        
+        [XmlText]
+        public string subtitleText { get; set; }
     }
 }
