3e2751382b2bd2e0b264c2d8ec7ed35079266318
Michael Christenson <mdc88@pdxchristenson.com>
2018-07-20 18:25:21 -0700

    Working version
    This is a working version of my test to deserialize an XML file

---
+++ XML_Deserialization/XML_Deserialization_Test/AddressDirectory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XML_Deserialization_Test
+{
+    public class AddressDirectory
+        {
+            [XmlElement("DirectoryOwner")]
+            public string DirectoryOwner { get; set; }
+            [XmlElement("PinCode")]
+            public string PinCode { get; set; }
+            [XmlElement("Address")]
+            public List<Address> Address { get; set; }
+            [XmlElement("Designation")]
+            public Designation designation { get; set; }
+        }
+}
