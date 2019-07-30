b2e2bd488a2b89eb650febd91f96cd393e18d026
mdc88 <mdc88@pdxchristenson.com>
2019-07-29 18:41:35 -0700

    Fix for NetworkStream.Flush
    Added an extension method for NetworkStream to clear anything out of the stream, so that we can make sure when we are sending Request and receiving Response that we aren't getting any garbage.
    
    Added a pragma rule for three warning messages in AcspTimelineExtension.cs (which we haven't implemented yet)
    
    Cleaned up another warning error.
    
    Added more documentation to various methods.
    
    Changed the Log level on some of the messages to Log.Verbose

---
+++ AcsListener/SharedCommon/NetworkStreamCleaner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCommon
+{
+    /// <summary>Helper extension class for clearing out a NetworkStream so that the next message can be read</summary>
+    public static class NetworkStreamCleaner
+    {
+        /// <summary>Clears the stream for next message.</summary>
+        /// <param name="inputStream">The input stream to be cleared</param>
+        /// <returns>total number of bytes read while clearing the input stream</returns>
+        public static int ClearStreamForNextMessage(this NetworkStream inputStream)
+        {
+            if (inputStream.DataAvailable)
+            {
+                int totalBytesRead = 0;
+                byte[] tempBuffer = new byte[1024];  // we don't actually care what goes in to the buffer, it is just a temporary holder
+
+                while (inputStream.DataAvailable)
+                {
+                    totalBytesRead += inputStream.Read(tempBuffer, 0, tempBuffer.Length);
+                }
+
+                return totalBytesRead;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
