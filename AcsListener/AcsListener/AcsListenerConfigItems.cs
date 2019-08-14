b39980009bf76ef1027e77efbf0a74da8f8f1b78
mdc88 <mdc88@pdxchristenson.com>
2019-08-14 16:57:24 -0700

    Fixes for issue #58
    Fixes #58

--- AcsListener/AcsListener/AcsListenerConfigItems.cs
+++ AcsListener/AcsListener/AcsListenerConfigItems.cs
@@ -19,25 +19,55 @@ namespace AcsListener
             Log.Debug("Checking the application config file for any default keys");
 
             // Get the CommandPort value
-            var keyValueCommandPort = ConfigurationManager.AppSettings.Get("CommandPort");
-            if (keyValueCommandPort != null)
+            try
             {
-                CommandPort = Convert.ToInt32(keyValueCommandPort);
-                Log.Debug($"Found CommandPort value: {CommandPort}");
+                var keyValueCommandPort = ConfigurationManager.AppSettings.Get("CommandPort");
+                if (keyValueCommandPort != null)
+                {
+                    CommandPort = Convert.ToInt32(keyValueCommandPort);
+                    Log.Debug($"Found CommandPort value: {CommandPort}");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Log.Error($"Error attempting to read the CommandPort configuration: {ex.Message}");
             }
 
             // Get the RplUrlPath
-            var keyValueRplUrlPath = ConfigurationManager.AppSettings.Get("RplUrlPath");
-            if (keyValueRplUrlPath != null)
+            try
             {
-                RplUrlPath = keyValueRplUrlPath;
-                Log.Debug($"Found RplUrlPath value: {RplUrlPath}");
+                var keyValueRplUrlPath = ConfigurationManager.AppSettings.Get("RplUrlPath");
+                if (keyValueRplUrlPath != null)
+                {
+                    RplUrlPath = keyValueRplUrlPath;
+                    Log.Debug($"Found RplUrlPath value: {RplUrlPath}");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Log.Error($"Error attempting to read the RplUrlPath configuration: {ex.Message}");
+            }
+
+            // Get the AutoReload value
+            try
+            {
+                var keyValueAutoReload = ConfigurationManager.AppSettings.Get("AutoReload");
+                if (keyValueAutoReload != null)
+                {
+                    AutoReload = Convert.ToBoolean(keyValueAutoReload);
+                    Log.Debug($"Found AutoReload value: {AutoReload}");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Log.Error($"Error attempting to read the AutoReload configuration: {ex.Message}");
             }
         }
 
-        public int CommandPort { get; set; } = 13000;  // default port of 13000, arbitrary choice on our part
-        public int AcsPort { get; } = 4170;            // default port of 4170, per SMPTE 430-10:2010 specifications, not a configurable value at this time
-        public string RplUrlPath { get; set; } = "";   // default path to the RPL files that are stored on the system in a website
+        public int CommandPort { get; set; } = 13000;    // default port of 13000, arbitrary choice on our part
+        public int AcsPort { get; } = 4170;              // default port of 4170, per SMPTE 430-10:2010 specifications, not a configurable value at this time
+        public string RplUrlPath { get; set; } = "";     // default path to the RPL files that are stored on the system in a website
+        public bool AutoReload { get; set; } = false;  // config item whether the "RELOAD" functionality requires manual invoke, or happens automatically
 
         /// <summary>Converts to string.</summary>
         /// <returns>A <see cref="System.String"/> that represents this instance.</returns>
